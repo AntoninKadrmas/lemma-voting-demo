4c6d5c8d946ff667182014a5521bc1ebf85cceea
Antonín Kadrmas <xkadrmas@fi.muni.cz>
2025-04-09 13:09:51 +0200

    fix (main): now it linting on my own machine

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-screen h-screen justify-center items-center">
-      <h1>For voting continue to the /voting?voteId='voteId'</h1>
+      <h1>For voting continue to the /voting?voteId=voteId</h1>
     </div>
   );
 }
